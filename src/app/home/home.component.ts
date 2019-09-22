fdc8ff1385dccb6efa858c2b01fec174466b9104
RKNyra <rk.nyra@gmail.com>
2019-09-23 01:44:18 +0300

    take data from the home component to display it in the quote-display component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Quote } from '../quote';
+
 
 @Component({
   selector: 'app-home',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  quotes: Quote[] = [
+    {quote:'They say a person needs just three things to be truly happy in this world: someone to love, something to do, and something to hope for.',author:'Tom Bodett',submittedBy:'Stl SuperG'},
+    {quote:'Wine is constant proof that God loves us and loves to see us happy.',author:'Benjamin Franklin',submittedBy:'RK Nyra'},
+    {quote:'If you think women are the weaker sex, try pulling the blanket back to your side',author:'Stuart Turner',submittedBy:'Judd Knight'},
+    {quote:'Have you noticed that all the people in favor of birth control are already born?',author:'Benny Hill',submittedBy:'Lisa Kempton'},
+    {quote:'Have you noticed that all the people in favor of birth control are already born?',author:'Benny Hill',submittedBy:'Lisa Kempton'},
+  ];
 
 showForm: boolean = false
 
@@ -21,6 +31,6 @@ showForm: boolean = false
     this.showForm = false
   }
   submitData(quote){
-
+console.log(quote);
   }
 }
