46ce092bad24896e6ed656ff94261dc09969f31f
RKNyra <rk.nyra@gmail.com>
2019-09-26 01:20:59 +0300

    modify the date-count pipe logicto try and fix the display of time elapsed since a quote was published

--- src/app/quote-display/quote-display.component.ts
+++ src/app/quote-display/quote-display.component.ts
@@ -5,7 +5,7 @@ import { Quote } from '../quote';
 @Component({
   selector: 'app-quote-display',
   templateUrl: './quote-display.component.html',
-  styleUrls: ['./quote-display.component.css']
+  styleUrls: ['./quote-display.component.css'],
 })
 export class QuoteDisplayComponent implements OnInit {
   @Input() quote : Quote
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,5 +1,4 @@
 export class Quote {
   constructor(public quote: string,public author: string,public submittedBy: string,public date: Date,public upvote?: number,public downvote?: number ) {
-
   }
 }
